8b6539c81b19d296d4b6112cdf04f5195d496d1c
Kshitij Gupta <guptakshitij2308@gmail.com>
2023-06-08 00:08:14 +0530

    Implemented second api endpoint for getting 3 days forecast with given coordinates (latitude and longitude)

--- app.js
+++ app.js
@@ -17,19 +17,19 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("/weather", (req, res) => {
+app.get("/weather/city", (req, res) => {
   const location = req.query.location;
   let units = req.query.units;
   if (!location) {
-    return res.status(400).json({ error: "Location parameter is missing" });
+    return res
+      .status(400)
+      .json({ error: "City whose weather info is needed is missing" });
   }
   if (!units) {
     units = "metric";
   }
 
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=${units}&appid=${apiKey}`;
-
-  apiUrl.replace("{city}", location);
 
   const request = https.get(apiUrl, (response) => {
     let data = "";
@@ -51,6 +51,66 @@ app.get("/weather", (req, res) => {
           "weather conditions": weatherData.weather,
         };
         // res.json(weatherData);
+        res.json(dataRet);
+      } else {
+        res.status(response.statusCode).json({ error: weatherData.message });
+      }
+    });
+  });
+
+  request.on("error", (error) => {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal server error" });
+  });
+});
+
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+
+app.get("/weather/coordinates", (req, res) => {
+  const lat = req.query.lat;
+  const lon = req.query.lon;
+  let units = req.query.units;
+  if (!lat || !lon) {
+    return res.status(400).json({
+      error: "Location parameters (latitude or longitude) is missing",
+    });
+  }
+  if (!units) {
+    units = "metric";
+  }
+
+  const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&cnt=3&units=${units}&appid=${apiKey}`;
+
+  const request = https.get(apiUrl, (response) => {
+    let data = "";
+
+    response.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    response.on("end", () => {
+      const weatherData = JSON.parse(data);
+      if (response.statusCode === 200) {
+        const dataRet = {
+          Location: {
+            "City Name": weatherData.city.name,
+            "Country Name": weatherData.city.country,
+          },
+          days: {
+            "Day 1": {
+              tempertaure: weatherData.list[0].main,
+              weather: weatherData.list[0].weather,
+            },
+            "Day 2": {
+              tempertaure: weatherData.list[1].main,
+              weather: weatherData.list[1].weather,
+            },
+            "Day 3": {
+              tempertaure: weatherData.list[2].main,
+              weather: weatherData.list[2].weather,
+            },
+          },
+        };
         res.json(dataRet);
       } else {
         res.status(response.statusCode).json({ error: weatherData.message });
